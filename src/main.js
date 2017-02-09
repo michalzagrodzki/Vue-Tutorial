2063e7f8f16fbc4cb14ddd9d8546d68ff8867b85
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-02-09 22:22:28 +0100

    Import component to main.js. App loads correctly now.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Home from './components/Home.vue'
 import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
